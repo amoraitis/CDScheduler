b88e03130e56b2c1b3c4e75456f34fb2cb7d63e0
amoraitis <tasosthira7@gmail.com>
2018-07-14 19:03:31 +0300

    Add project files.

---
+++ CDScheduler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CDScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
